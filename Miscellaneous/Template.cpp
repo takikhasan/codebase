2462916b1fbce8bf5e11a4d10b2f44f7988a7dfe
Takik Hasan <hasantakik@gmail.com>
2020-06-16 03:02:58 +0600

    Update Template.cpp

--- Miscellaneous/Template.cpp
+++ Miscellaneous/Template.cpp
@@ -67,14 +67,14 @@ template<typename T> inline void I(vector<T> &a) { for (T &aa : a) I(aa); }
 template<typename T, typename... Args> inline void I(T &a, Args &... args) { I(a); I(args...); }
 
 
-inline void OUT(int a) { printf("%d", a); }
-inline void OUT(LL a) { printf("%lld", a); }
-inline void OUT(ULL a) { printf("%llu", a); }
-inline void OUT(const char* a) { printf("%s", a); }
-inline void OUT(char* a) { printf("%s", a); }
-inline void OUT(char a) { printf("%c", a); }
-inline void OUT(string a) { rep(it, a.begin(), a.end()) printf("%c", *it); }
-inline void OUT(bool a) { printf("%d", a); }
+inline void OUT(const int &a) { printf("%d", a); }
+inline void OUT(const LL &a) { printf("%lld", a); }
+inline void OUT(const ULL &a) { printf("%llu", a); }
+inline void OUT(const char *a) { printf("%s", a); }
+inline void OUT(char *a) { printf("%s", a); }
+inline void OUT(const char &a) { printf("%c", a); }
+inline void OUT(const string &a) { rep(it, a.begin(), a.end()) printf("%c", *it); }
+inline void OUT(const bool &a) { printf("%d", a); }
 template<typename T, typename... Args> inline void OUT(T a, Args... args) { OUT(a); OUT(" "); OUT(args...); }
 template<typename... Args> inline void O(Args... args) { OUT(args...); OUT("\n"); }
 

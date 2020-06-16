2299ac60c020b51b83e9aaa4c3694a60ff22e203
Takik Hasan <hasantakik@gmail.com>
2020-06-16 06:11:36 +0600

    Update Template.cpp

--- Miscellaneous/Template.cpp
+++ Miscellaneous/Template.cpp
@@ -83,12 +83,23 @@ template<typename T>
 inline ostream& operator<<(ostream& os, vector<T>& a) { os << "["; for (int i = 0; i < (int)a.size(); i++) { if (i) os << ", "; os << a[i]; } os << "]"; return os; }
 
 
-#define error(args...) { string _s = #args; replace(_s.begin(), _s.end(), ',', ' '); stringstream _ss(_s); istream_iterator<string> _it(_ss); err(_it, args); }
-void err(istream_iterator<string> it) {}
+int DEBUG_LINE = 0;
+#define error(args...) { cout << "[DEBUG] Line " << DEBUG_LINE++ << ": "; string _s = #args; err(_s, 0, args); }
+void err(const string &name, int in) {}
 template<typename T, typename... Args>
-void err(istream_iterator<string> it, T a, Args... args) {
-	cout << *it << " = " << a << endl;
-	err(++it, args...);
+void err(const string &name, int in, T a, Args... args) {
+    if (name[in] == ' ') in++; string curr_name;
+    while (in < SZ(name) && name[in] != ',') {
+        if (name[in] == 34 || name[in] == 39) {
+            curr_name.pb(name[in]);
+            char c = name[in++];
+            while (name[in] != c) curr_name.pb(name[in++]);
+        }
+        curr_name.pb(name[in++]);
+    }
+    if (curr_name.back() == ' ') curr_name.pop_back();
+	cout << curr_name << " = " << a << (sizeof...(args) ? ", " : "\n");
+	err(name, ++in, args...);
 }
 
 

2a9ebc5351230d0f778bfb754c8cd4e3b9c3b6a6
Takik Hasan <hasantakik@gmail.com>
2020-09-10 16:09:01 +0600

    Update MO's Algorithm on Trees (Path Query).cpp

--- Data Structure/MO's Algorithm on Trees (Path Query).cpp
+++ Data Structure/MO's Algorithm on Trees (Path Query).cpp
@@ -1,148 +1,70 @@
 /*
-    #1. T1 -> Data type required for query
-    #2. T2 -> Data type required for edge weight
-    #3. Introduced two data types for memory efficiency
+    #1. An LCA Template that gives us O(1) per lca(a, b) query.
+    #2. Using that, we can get a few queries like path sum query in O(1).
+    #3. Source:
+        i.  https://github.com/kth-competitive-programming/kactl/blob/master/content/graph/LCA.h
+    #4. Problems (Easy to Hard):
+        i.  https://vjudge.net/solution/27176384
 */
-template<typename T1, typename T2>
-struct LCA
-{
-    typedef pair<int, T2> ii;
-    const int MIN = 1, MAX = 2, SUM = 3, GCD = 4;
-    /* 0 indexed */
-    int n;
-    vector<int> l;
-    vector<vector<int>> p;
-    vector<vector<T1>> val;
-    int OPTION;
-    T1 OPTION_VAL;
-    /* parent, val, option */
-    int LOG;
-    vector<int> logs, powr;
-    vector<vector<ii>> g;
-    LCA(int n, int option) : n(n), l(n), g(n), OPTION(option)
-    {
-        LOG = log2(n) + 1;
-        OPTION_VAL = (OPTION == SUM || option == GCD) ? 0 : ((OPTION == MIN) ? numeric_limits<T1>::max() : numeric_limits<T1>::min());
-    }
-    LCA(int n, vector<vector<ii>> g, int option) : n(n), l(n), g(g), OPTION(option)
-    {
-        LOG = log2(n) + 1;
-        OPTION_VAL = (OPTION == SUM || option == GCD) ? 0 : ((OPTION == MIN) ? numeric_limits<T1>::max() : numeric_limits<T1>::min());
-    }
-    void addEdge(int u, int v, T2 w)
-    {
-        g[u].pb(mp(v, w));
-//        g[v].pb(mp(u, w));
-    }
-    void dfs(int u, int pp, int ll)
-    {
-        l[u] = ll++;
-        p[u][0] = pp;
-        for (ii x : g[u]) {
-            if (x.ff != pp) {
-                val[x.ff][0] = x.ss;
-                dfs(x.ff, u, ll);
-            }
-        }
-    }
-    T1 func(T1 x, T2 y)
-    {
-//        return x + y;
-//        return min(x, y);
-//        return max(x, y);
-        return __gcd(x, y);
-    }
-    void build()
-    {
-        p = vector<vector<int>> (n, vector<int> (LOG));
-        val = vector<vector<T1>> (n, vector<T1> (LOG));
-        dfs(0, -1, 0);
-        for (int i = 1; i < LOG; i++) {
-            for (int j = 0; j < n; j++) {
-                if (p[j][i-1] != -1) {
-                    val[j][i] = func(val[j][i-1], val[p[j][i-1]][i-1]);
-                    p[j][i] = p[p[j][i-1]][i-1];
-                }
-                else {
-                    p[j][i] = -1;
-                    val[j][i] = OPTION_VAL;
-                }
-            }
-        }
-    }
-    int lca(int u, int v)
-    {
-        if (l[u] > l[v]) swap(u, v);
-        int d = l[v] - l[u];
-        for (int i = 0; i < LOG; i++) {
-            if (CHECK(d, i)) {
-                v = p[v][i];
-            }
-        }
-        if (u == v) return u;
-        for (int i = LOG - 1; i >= 0; i--) {
-            if (p[u][i] != p[v][i]) {
-                u = p[u][i];
-                v = p[v][i];
-            }
-        }
-        return p[u][0];
-    }
-    T1 query(int u, int v)
-    {
-        T1 ret = OPTION_VAL;
-        if (l[u] > l[v]) swap(u, v);
-        int d = l[v] - l[u];
-        for (int i = 0; i < LOG; i++) {
-            if (CHECK(d, i)) {
-                ret = func(ret, val[v][i]);
-                v = p[v][i];
-            }
-        }
-        if (u == v) return ret;
-        for (int i = LOG - 1; i >= 0; i--) {
-            if (p[u][i] != p[v][i]) {
-                ret = func(ret, val[u][i]);
-                ret = func(ret, val[v][i]);
-                u = p[u][i];
-                v = p[v][i];
-            }
-        }
-        ret = func(ret, val[u][0]);
-        ret = func(ret, val[v][0]);
-        return ret;
-    }
-    /*
-        To use array queries:
 
-            Initialize for an array a[n]:
-                LCA<T1, T2> lca(n + 1); // T1 -> Data type for Query, T2 -> Data type for array element
-                for (int i = 0; i < n; i++) {
-                    lca.addEdge(i, i + 1, a[i]);
-                }
-                lca.build();
-                lca.fast_g(); // Only for O(1) queries
+#define rep(i, a, b) for(int i = a; i < (b); ++i)
 
-            For query(l, r) where l and r are 0-indexed but any kind of query
-            including O(logn) and O(1) ones:
-                O(logn):
-                    cout << lca.query(l, r + 1) << endl;
-                O(1):
-                    cout << lca.fast_query(l, r + 1) << endl;
+template<class T>
+struct RMQ {
+	vector<vector<T>> jmp;
 
+	RMQ(const vector<T>& V) : jmp(1, V) {
+		for (int pw = 1, k = 1; pw * 2 <= SZ(V); pw *= 2, ++k) {
+			jmp.emplace_back(SZ(V) - pw * 2 + 1);
+			rep(j, 0, SZ(jmp[k]))
+				jmp[k][j] = min(jmp[k - 1][j], jmp[k - 1][j + pw]);
+		}
+	}
+
+	T query(int a, int b) {
+		assert(a < b); // or return inf if a == b
+		int dep = 31 - __builtin_clz(b - a);
+		return min(jmp[dep][a], jmp[dep][b - (1 << dep)]);
+	}
+};
+
+struct LCA {
+	int T = 0;
+	VI time, path, ret;
+	VI dist;
+	RMQ<int> rmq;
+
+	LCA(vector<vector<pii>>& C) : dist(SZ(C)), time(SZ(C)), rmq((dfs(C, 0, -1, 0), ret)) {}
+
+	void dfs(vector<vector<pii>> &C, int v, int par, int curr_sum) {
+		time[v] = T++;
+		dist[v] = curr_sum;
+		for (pii p : C[v]) {
+            int w = p.ss, y = p.ff;
+            if (y != par) {
+                path.push_back(v), ret.push_back(time[v]);
+                dfs(C, y, v, curr_sum + w);
+            }
+		}
+	}
+
+	int lca(int a, int b) {
+		if (a == b) return a;
+		tie(a, b) = minmax(time[a], time[b]);
+		return path[rmq.query(a, b)];
+	}
+
+	int distt(int a, int b) { return dist[a] + dist[b] - 2 * dist[lca(a,b)]; }
+	/**
+        When weight is in nodes (For this template and the other),
+            Tree creation:
+                Just add edges like this:
+                    For edge (a->b),
+                        tree[a].push_back(b, weight[b]);
+                        tree[b].push_back(a, weight[a]);
+            Query:
+                Handle lca(a, b) separately.
     */
-    void fast_g()
-    {
-        logs.resize(n); powr.resize(LOG);
-        logs[1] = 0;    powr[0] = 1;
-        for (int i = 1; i < LOG; i++)   powr[i] = powr[i-1] * 2;
-        for (int i = 2; i < n  ; i++)   logs[i] = logs[i/2] + 1;
-    }
-    T1 fast_query(int l, int r)
-    {
-        int j = logs[r-l];
-        return func(val[r][j], val[l+powr[j]][j]);
-    }
 };
 
 /*
@@ -255,8 +177,7 @@ namespace MO_Tree_Path
         }
 
         /** Second, initialize LCA */
-        LCA<int, int> G(n, tree, 1);
-        G.build();
+        LCA G(tree);
 
         /** Third, use LCA to turn queries into sub-arrays */
         for (Query &q : queries) {
